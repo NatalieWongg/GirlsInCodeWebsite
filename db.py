7a08154b5841564adb2a1e15b81c1f7e8c615a3a
CameliaCH <cameliachew@icloud.com>
2025-11-28 22:26:07 +0800

    edits

--- db.py
+++ db.py
@@ -5,6 +5,6 @@ from supabase import create_client
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
-supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
